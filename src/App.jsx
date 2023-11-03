36018c58a8022fca28209eb7088faa36a65971e8
Mohammad-Asraful <asrafmail01@gmail.com>
2023-11-03 21:45:36 +0600

    Design part running

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import products from "./image";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <section>
+      <div className="container mx-auto py-10 px-4">
+        {/* Wrapping div start */}
+        <div className="bg-[#F7F7F7] p-2">
+          {/* header div start */}
+          <div className="flex justify-between bg-white py-3 px-5 rounded-t-md">
+            <div>
+              <input defaultChecked type="checkbox" /> 3 File Selected
+            </div>
+            <div className="text-red-600 font-semibold">Delete files</div>
+          </div>
+          {/* header div end */}
 
-export default App
+          {/* hr line start */}
+          <hr className="bg-gray-400 w-full h-px" />
+          {/* hr line end */}
+
+          {/* Images div start */}
+          <div className="grid grid-cols-5 gap-5 bg-white py-3 px-5 rounded-b-md [&>*:first-child]:row-span-2 [&>*:first-child]:col-span-2">
+            {/* image */}
+            {products.map((product) => {
+              return (
+                <div key={product.id}>
+                  <img
+                    className="rounded-md border border-gray-400 cursor-move"
+                    src={product.imageURL}
+                    alt="Blue colour Headphone image"
+                  />
+                </div>
+              );
+            })}
+            {/* image */}
+
+            {/* file upload input start*/}
+            <div>Upload your file</div>
+            {/* file upload input end*/}
+          </div>
+          {/* Images div end */}
+        </div>
+        {/* Wrapping div end */}
+      </div>
+    </section>
+  );
+};
+
+export default App;
---
+++ src/image.js
@@ -0,0 +1,47 @@
+const products = [
+  {
+    id: 1,
+    imageURL: "image-1.webp",
+  },
+  {
+    id: 2,
+    imageURL: "image-2.webp",
+  },
+  {
+    id: 3,
+    imageURL: "image-3.webp",
+  },
+  {
+    id: 4,
+    imageURL: "image-4.webp",
+  },
+  {
+    id: 5,
+    imageURL: "image-5.webp",
+  },
+  {
+    id: 6,
+    imageURL: "image-6.webp",
+  },
+  {
+    id: 7,
+    imageURL: "image-7.webp",
+  },
+  {
+    id: 8,
+    imageURL: "image-8.webp",
+  },
+  {
+    id: 9,
+    imageURL: "image-9.webp",
+  },
+  {
+    id: 10,
+    imageURL: "image-10.jpeg",
+  },
+  {
+    id: 11,
+    imageURL: "image-11.jpeg",
+  },
+];
+export default products;
